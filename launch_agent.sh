ac45048efe7c16653b29613eff0499a6b9297f82
Gabriel Puigdemunt <gabrielpuigdemunt@gmail.com>
2023-09-15 12:24:06 +0200

    Update launch_agent.sh

--- launch_agent.sh
+++ launch_agent.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# mvn clean install -f ${SCRIPT_DIR}/pom.xml
 
+mvn -f ${SCRIPT_DIR}/pom.xml clean install
 mvn -f ${SCRIPT_DIR}/pom.xml exec:java
